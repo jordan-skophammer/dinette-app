bf4fe28ed53b27a8422e356228336f9b20de7355
Jordan Skophammer <jordan.skophammer@gmail.com>
2018-07-07 12:06:30 -0500

    fixed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,37 +2,20 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/Ballot";
-import Blog from "./pages/Login";
-import Contact from "./pages/Search";
+import Ballot from "./pages/Ballot";
+import Login  from "./pages/Login";
+import Search from "./pages/Search";
 import User from "./pages/User"
 import './App.css';
-import logo from './logo.svg';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Home/>
-      </div>
-    );
-  }
-}
+// import logo from './logo.svg';
 
 const App = () => (
   <Router>
     <div>
       <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/blog" component={Blog} />
-      <Route path="/contact" component={Contact} />
+      <Route path="/Ballot" component={Ballot} />
+      <Route path="/Login" component={Login} />
+      <Route path="/Search" component={Search} />
       <Route path="/user" component={User} />
     </div>
   </Router>
