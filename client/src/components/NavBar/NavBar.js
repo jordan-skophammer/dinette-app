38f2f9cacba5e5ee2c367db5ddfa9eeb6b428868
Kathy  Varble <kathyvarble@Kathys-MacBook-Pro.local>
2018-07-07 12:42:40 -0500

    new navbar

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,30 +1,26 @@
 import React from "react";
 import "./NavBar.css";
 
-const NavBar = props => <Navbar inverse collapseOnSelect>
-<Navbar.Header>
-  <Navbar.Brand>
-    <a href="#brand">Dinette!</a>
-  </Navbar.Brand>
-  <Navbar.Toggle />
-</Navbar.Header>
-<Navbar.Collapse>
-  <Nav>
-    <NavItem eventKey={1} href="#">
-      Search
-    </NavItem>
-    <NavItem eventKey={2} href="#">
-      Vote
-    </NavItem>
-    <NavItem eventKey={2} href="#">
-      Roulette
-    </NavItem>
-  </Nav>
-  <Nav pullRight>
-    <NavItem eventKey={1} href="#">
-      Log Out
-    </NavItem>
-  </Nav>
-</Navbar.Collapse>
-</Navbar>;
+const NavBar = props => (
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#">Dinette</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+    <li className="nav-item active">
+        <a className="nav-link" href="#">Search <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Vote <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Roulette <span className="sr-only">(current)</span></a>
+      </li>
+    </ul>
+  </div>
+</nav>
+)
 export default NavBar;
