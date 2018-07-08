cbb738f866ef1714a542eb54fc1507753eb31d9f
Mary Quint <mary.k.quint@gmail.com>
2018-07-08 18:58:21 -0500

    added props for search results

---
+++ client/src/components/SearchResults/SearchResults.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SearchResults = props => {
+    return (
+        <div>
+            <h3>{props.restaurantName}</h3>
+            <p>{props.address}</p>
+            <hr/>
+        </div>
+    )
+}
+
+export default SearchResults;
--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./Search.css"
 import API from "../../utils/API"
 import NavBar from "../../components/NavBar"
+import SearchResults from "../../components/SearchResults"
 
 class Search extends Component {
 
@@ -12,7 +13,7 @@ class Search extends Component {
     search = () => {
         API.searchRestaurants()
         .then(res => {
-            this.setState({results: res})
+            this.setState({results: res.results})
             console.log(res)
         })
     }
@@ -26,8 +27,15 @@ class Search extends Component {
         <div>
             <NavBar/>
             <br/>
-            <h1>Search Page</h1>
-            <h2>Other components</h2>
+            <h2>Search Results</h2>
+            {this.state.results.map(restaurant => (
+                <SearchResults
+                    key = {restaurant.id}
+                    restaurantName = {restaurant.name}
+                    address = {restaurant.vicinity}
+                    image = {restaurant.photos.html_attribution}
+                />
+            ))}
         </div>
 
         )
