df8d5e9016fc65821953c2f343618b00b0227d19
Jordan Skophammer <jordan.skophammer@gmail.com>
2018-07-09 05:28:26 -0500

    changed user schema

--- models/user.js
+++ models/user.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  userName: { type: String, required: true },
-  password: { type: Date, required: true},
+  userName: {type: String, required: true },
+  password: {type: Date, required: true},
+  firstName:{type: String, required: true},
+  lastName:{type: String, required: true},
   zipCode: {type: Number, min: 5, max:5, required: true}
 });
 
