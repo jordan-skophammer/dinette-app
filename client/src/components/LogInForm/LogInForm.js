75d0889e69540041ea32688487a4837cbc1b7c51
Jordan Skophammer <jordan.skophammer@gmail.com>
2018-07-13 19:53:24 -0500

    changed login page style

--- client/src/components/LogInForm/LogInForm.js
+++ client/src/components/LogInForm/LogInForm.js
@@ -2,21 +2,24 @@ import React from "react";
 import "./LogInForm.css"
 
 const LogInForm = (props) => (
-    <div className="card">
-        <div className="card-body">
-            <form>
-                <div className="form-group">
-                    <label htmlFor="loginEmail">Email/Username</label>
-                    <input type="email" className="form-control" id="loginEmail" placeholder="Enter email" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="loginPassword">Password</label>
-                    <input type="password" className="form-control" id="loginPassword" placeholder="Enter Password" />
-                </div>
-                <button type="submit" className="btn btn-danger">Login</button>
-            </form>
-            <a href="/signup" className="SignUp"><button className="btn btn-outline-danger">Create User</button></a>
-        </div>  
+    <div className="row justify-content-center">
+        <div className="card login-card">
+            <div className="card-body">
+                <form>
+                    <div className="form-group">
+                        <label className="font-weight-bold" htmlFor="loginEmail">Email/Username</label>
+                        <input type="email" className="form-control" id="loginEmail" placeholder="Enter email" />
+                    </div>
+                    <div className="form-group">
+                        <label className="font-weight-bold" htmlFor="loginPassword">Password</label>
+                        <input type="password" className="form-control" id="loginPassword" placeholder="Enter Password" />
+                    </div>
+                    <button type="submit" className="btn btn-danger">Login</button>
+                </form>
+                <br/>
+                <a href="/signup" className="SignUp"><button className="btn text-white create-button">Create User</button></a>
+            </div>  
+        </div>
     </div>
 )
 
