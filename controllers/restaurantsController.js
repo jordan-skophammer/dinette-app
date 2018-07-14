922a2e765992246746351c4cc8f542f5e59581a5
unknown <peter.alan.anderson@gmail.com>
2018-07-14 12:00:19 -0500

    search details return

--- controllers/restaurantsController.js
+++ controllers/restaurantsController.js
@@ -1,11 +1,19 @@
 const axios = require ("axios")
 const nearbyQueryStringA = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="
 const nearbyQueryStringB = "&radius=10000&type=restaurant&key=AIzaSyCJ2pazcdZHkXUkCyXNzV2iwXPCex7ODdY"
-const geolocateQueryString = "https://maps.googleapis.com/maps/api/geocode/json?address=Mall+of+America&key=AIzaSyCB5tndG-nx3Z8RR-fnmeyXrEgkTRhYqSs"
+const geolocateQueryStringA = "https://maps.googleapis.com/maps/api/geocode/json?address="
+const geolocateQueryStringB = "&key=AIzaSyCB5tndG-nx3Z8RR-fnmeyXrEgkTRhYqSs"
+const placesDetailsQueryStringA = "https://maps.googleapis.com/maps/api/place/details/json?placeid="
+const placesDetailsQueryStringB = "&fields=name,rating,address_component,photo,type,formatted_phone_number,opening_hours,review&key=AIzaSyCJ2pazcdZHkXUkCyXNzV2iwXPCex7ODdY"
+
+restaurantsArr = []
 
 
 module.exports = {
     search: (req, res) => {
+        let geolocateQueryString = geolocateQueryStringA
+        geolocateQueryString += "Mall+of+America"
+        geolocateQueryString += geolocateQueryStringB
         axios.get(geolocateQueryString)
             .then(function(data){
                 console.log(data.data.results[0])
@@ -17,12 +25,28 @@ module.exports = {
                 nearbyStringFull += lng
                 nearbyStringFull += nearbyQueryStringB
                 axios.get(nearbyStringFull)
-                    .then(data => res.send(data.data.results))
-                    // .then(data=>console.log(data.data))
-            })
-        
-
+                    .then(function(data){
+                        console.log(data.data.results)
+                        let noDetailsDataArr = []
+                        noDetailsDataArr = data.data.results
+                        getDetailsAddToArray(noDetailsDataArr, res)
+                    }
+            )
+        })
     }
+}
 
-    
+function getDetailsAddToArray(array, res){
+    if (array.length == 0) res.send(restaurantsArr)
+    let element = array.pop()
+    let queryString = placesDetailsQueryStringA
+    queryString += element.place_id
+    queryString += placesDetailsQueryStringB
+    console.log("QUERY STRING",queryString)
+    axios.get(queryString)
+        .then(function(data){
+            console.log("data: ",data.data)
+            restaurantsArr.push(data.data)
+            getDetailsAddToArray(array, res)
+        })
 }
