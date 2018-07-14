d1aa6544626bcc6e230fa231d487d7ffb36a9c36
Mary Quint <mary.k.quint@gmail.com>
2018-07-13 22:23:28 -0500

    got rid of unused classes and unnecessary css

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -92,7 +92,7 @@ class Search extends Component {
                                     <input type="text" className="form-control" id="searchLocation" value={this.state.value} onChange={this.handleChange} placeholder="Search by ZIP or landmark"></input>
                                 </div>
                                 <div className="col-sm-3">
-                                    <button className="btn btn-lg save text-white" id="searchLocation" onClick={this.searchLocation}>Search</button>
+                                    <button className="btn btn-lg save text-white yellow" id="searchLocation" onClick={this.searchLocation}>Search</button>
                                 </div>
                             </div>
                         </form>
@@ -101,8 +101,7 @@ class Search extends Component {
                 </div>
                 <div className="row">
 
-                <div className="col-md-12 results-card orange">
-                        {/* <div className="results-card"> */}
+                    <div className="col-md-12 results-card orange">
                             <h3 className="text-white text-center">Search Results</h3>
                             <br/>
                                 
@@ -114,17 +113,11 @@ class Search extends Component {
                                                 <p className="address">{restaurant.vicinity}</p>
                                             </label>
                                             <input className="form-check-input" data-state="unchecked" type="checkbox" onClick= {() => this.addToSessionStorage(restaurant.name)} value={restaurant.name} id="defaultCheck"></input>
-
-                                        {/* {restaurant.photos[0].html_attribution} */}
-                                        <br/>
                                         </div>
 
                                     </div>
 
                                 ))}
-                            
-                        {/* </div> */}
-                        {/* <br/> */}
                     </div>
                 </div>
                         <div className="col-md-12">
