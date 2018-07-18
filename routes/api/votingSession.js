d1895e4bdd9981918eaaacf65b672ffbedcb6333
Jordan Skophammer <jordan.skophammer@gmail.com>
2018-07-18 18:33:43 -0500

    roulette page fix

---
+++ routes/api/votingSession.js
@@ -0,0 +1,27 @@
+const router = require ("express").Router();
+const axios = require ("axios")
+const firebase = require ("firebase")
+
+var config = {
+    apiKey: "AIzaSyBzRymxfiS_lpwfG8qUu8CZKEOTdqQmoC4",
+    authDomain: "dinette-1530978117932.firebaseapp.com",
+    databaseURL: "https://dinette-1530978117932.firebaseio.com/",
+    projectId: "dinette-1530978117932",
+    storageBucket: "dinette-1530978117932.appspot.com",
+    messagingSenderId: "273423651260"
+  };
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+router.post("/",function(req, res){
+    console.log("FIREBASE")
+    database.ref().set({
+        TEST: "Hello World!"
+    })
+}
+    
+)
+
+module.exports = router;
