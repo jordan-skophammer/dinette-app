f7db88421120f807cae7e6c6b8b011ef9223ebad
Jordan Skophammer <jordan.skophammer@gmail.com>
2018-07-24 06:47:30 -0500

    more deployment changes

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.use(routes);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('client/public'));
 }
 
 // error handler
