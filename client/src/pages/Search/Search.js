297a20411a3f26a46a1c2e18aca927d943a5b1ad
unknown <peter.alan.anderson@gmail.com>
2018-07-25 02:59:52 -0500

    votes log and return key

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -103,7 +103,7 @@ class Search extends Component {
         console.log(restaurantsArray, ' is restaurantsArray')
         let voteObject = {
 
-            username: "test owner",
+            username: userName,
             restaurantsArr: restaurantsArray
         }
         console.log("vote session info:",this.props.firstName)
