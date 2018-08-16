502d748b7bc5f6c4afbb029ca3c36c81327502d5
Jordan Skophammer <jordan.skophammer@gmail.com>
2018-08-15 21:32:52 -0500

    moblie loading fix

--- routes/api/searchLocation.js
+++ routes/api/searchLocation.js
@@ -25,11 +25,11 @@ const placesDetailsQueryStringB = "&fields=name,rating,address_component,photo,t
 
 //Geolocation
 
-// getLocation("https://geoip-db.com/json", function (error, response, body) {
-//     console.log('error:', error); // Print the error if one occurred
-//     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//     console.log('body:', body); // Print the HTML for the Google homepage.
-// });
+getLocation("https://geoip-db.com/json", function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+});
 
 // Global variable to store results (via closure)
 let restaurantsArr = []
